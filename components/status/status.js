66a2f0380598148183e7eba0f411e2e78cb96e23
Kevina <kevinanakayiza@gmail.com>
2025-11-21 17:31:25 +0300

    have created a status component that holds the the cards

---
+++ components/status/status.js
@@ -0,0 +1,71 @@
+ 
+
+  const card = document.querySelector(".card")
+
+ 
+
+  const carddetail = (options) =>{
+    const template = document.createElement("template")
+    template.innerHTML = `
+    <div class="contianer">
+    <div class="card__uppersection"> 
+    <p> ${options.title}</p>
+    <div>
+     <img src= "${options.icons}">
+    </div>
+  </div>
+  <div>
+    <p>${options.number}</p>
+    <p>${options.text}</p>
+  </div> 
+
+  </div>
+         
+    `
+    return template.content.cloneNode(true);
+  }
+
+ 
+
+  const getdeatils = carddetail ( {
+    title: "active Tasks",
+    icons: "/assets/images/checkedbox.svg",
+    number: 2,
+    text: "0 completed"
+  })
+
+  const card2 = carddetail({
+    title: "Total Expenses",
+    icons: "/assets/images/dollar.svg",
+    number: "$0.00",
+    text: "This period"
+
+  })
+
+  const card3 = carddetail({
+    title: "Total Notes",
+    icons: "/assets/images/file.svg",
+    number: "0",
+    text: "Saved notes"
+
+  })
+
+
+  const card4 = carddetail({
+    title: "Completion",
+    icons: "/assets/images/dollar.svg",
+    number: "0%",
+    text: "Task Completion"
+
+  })
+  
+
+  
+card.appendChild(getdeatils);
+card.appendChild(card2);
+card.appendChild(card3);
+card.appendChild(card4);
+
+
+
+  
